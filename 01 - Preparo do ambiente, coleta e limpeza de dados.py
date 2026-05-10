6875847a139824c6e55e8feb6c06297cc60ac4af
José Valdeir <valdeircomv@gmail.com>
2026-05-10 00:53:36 -0300

    01 atualizado!

--- 01 - Preparo do ambiente, coleta e limpeza de dados.py
+++ 01 - Preparo do ambiente, coleta e limpeza de dados.py
@@ -1,4 +1,23 @@
-#Preparo do ambiente, coleta e limpeza de dados
+# Preparo do ambiente, coleta e limpeza de dados
+
+# Antes de mais nada é necessário criar um ambiente no qual faremos a instalação das bibliotecas e configurações necessárias.
+# Considerando que o python está instalado. Vamos a passo a passo:
+
+# Passo 1, criar uma máquina virtual:
+#    Pelo CMD, entre numa pasta específica > use o comando "python -m venv NomeDaMaquinaVirtual" para criar uma VM
+
+# Passo 2, ativar a máquina virtual que foi criada:
+#    No CMD, acesse a pasta criada e vá até a pasta "Scripts"
+#    Dentro da pasta Scripts, pelo CMD, dê o comando "activate"
+#    Agora a VM está criada, acessada e pronta para uso, mas sem as bibliotecas
+
+# Passo 3, instalando bibliotecas:
+#    Ainda dentro da VM, pelo CMD, pode dar os comandos "pip install nomeDaBiblioteca" que vai instalar a biblioteca que você quiser
+#    Se os nomes das bilbiotecas estiverem em um arquivo TXT, pode usar "pip install "pip install -r requirements.txt"
+
+
+# Pronto, ambiente configurado!
+# Agora basta importar os dados para um dataframe e começar alimpar esses dados.
 
 
 
